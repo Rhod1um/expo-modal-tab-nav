80ab68b3425ff4ef1a310be485d57c489e37a57e
Veronica Frydkjær <frida-nanni@hotmail.com>
2023-10-29 22:58:26 +0100

    hvad der blev nået til sidste sps

--- app/(tabs)/map.js
+++ app/(tabs)/map.js
@@ -1,88 +1,60 @@
-import { Stack, useRouter } from "expo-router";
-import { StyleSheet, Text, View, Button, Platform, FlatList, Image } from "react-native";
-import { useState, useEffect } from "react"; //vi skal bruge state til at håndtere posts
-import MapView from 'react-native-maps'
-
+import { Stack, useFocusEffect, useRouter } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { Button, Image, Platform, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function Map() {
-    const [posts, setPosts] = useState([]);
     const router = useRouter();
-
-    const API_URL = "https://expo-post-app-49c88-default-rtdb.europe-west1.firebasedatabase.app";
+    const [posts, setPosts] = useState([]);
+    const API_URL = "https://expo-post-app-default-rtdb.firebaseio.com";
 
     useEffect(() => {
         getPosts();
     }, []);
 
+    // Sometimes we want to run side-effects when a screen is focused.
+    // https://reactnavigation.org/docs/use-focus-effect/
+    useFocusEffect(
+        // If you don't wrap your effect in React.useCallback, the effect will run every render if the screen is focused.
+        useCallback(() => {
+            getPosts();
+        }, [])
+    );
+
     async function getPosts() {
-        const response = await fetch(`${API_URL}/posts.json`); // Fetch posts data from the specified API endpoint
-        const dataObj = await response.json();   // Parse the response data into an object
-      
-        // Convert the data object into an array of posts with 'id' properties
-        const postsArray = Object.keys(dataObj).map(key => ({ id: key, ...dataObj[key] })); // from object to array
-      
-        // Sort the posts in descending order based on their 'createdAt' property
+        const response = await fetch(`${API_URL}/posts.json`);
+        const dataObj = await response.json();
+        const postsArray = Object.keys(dataObj).map(key => ({
+            id: key,
+            ...dataObj[key]
+        })); // from object to array
         postsArray.sort((postA, postB) => postB.createdAt - postA.createdAt); // sort by timestamp/ createdBy
-      
-        setPosts(postsArray);  // Set the sorted posts in your application's state
-
-        console.log(postsArray)
-      }
-
-    function showCreateModal() {
-        router.push("/create");
-    }
-
-    function renderPost(item) {
-        const post = item.item;
-        return (
-					<View style={styles.postContainer}>
-            <Image style={styles.image} source={{ uri: post.image }} />
-            <Text style={styles.caption}>{post.caption}</Text>
-        </View>
-			);
+        setPosts(postsArray);
     }
 
     return (
-        <View style={styles.list}>
-            <Stack.Screen
-                options={{
-                    headerRight: () => (
-                        <Button
-                            title="Add New"
-                            color={Platform.OS === "ios" ? "#fff" : "#264c59"}
-                            onPress={() => router.push("/post-modal")}
-                        />
-                    )
-                }}
-            />
-
-            <FlatList
-                data={posts}
-                renderItem={renderPost}
-                keyExtractor={post => post.id}
-            />
+        <View style={styles.container}>
+            <MapView style={styles.map}>
+                {posts.map(post => (
+                    <Marker
+                        key={post.id}
+                        coordinate={post.location}
+                        title={post.caption}
+                    />
+                ))}
+            </MapView>
         </View>
     );
 }
 
+// styling
+
 const styles = StyleSheet.create({
-    list: {
+    container: {
         flex: 1
     },
-    postContainer: {
-        flex: 1,
-        minHeight: 320,
-        paddingBottom: 30,
-        borderBottomColor: "#acc6c9",
-        borderBottomWidth: 0.5
-    },
-    caption: {
-        fontSize: 22,
-        padding: 15
-    },
-    image: {
-        aspectRatio: 1,
-        flex: 1
+    map: {
+        width: "100%",
+        height: "100%"
     }
 });
